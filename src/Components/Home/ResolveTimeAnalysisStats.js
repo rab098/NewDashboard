ad1ee3de93e041240220da5e20604c92eb9fdfb0
rab098 <rabiasehar98@gmail.com>
2020-07-20 15:47:13 +0500

    pull

--- src/Components/Home/ResolveTimeAnalysisStats.js
+++ src/Components/Home/ResolveTimeAnalysisStats.js
@@ -5,6 +5,8 @@ import ChartistGraph from "react-chartist";
 import Box from "@material-ui/core/Box";
 import "../../ComponentsCss/resolveTimeAnalysis.css";
 
+let store = require("store");
+
 let Chartist = require("chartist");
 
 function ResolveTimeAnalysisStats() {
@@ -12,47 +14,54 @@ function ResolveTimeAnalysisStats() {
     less: 70,
     medium: 20,
     high: 5,
-      greaterThanTwentyFour: 5
+    greaterThanTwentyFour: 5,
   });
 
+  const [resolveTimeCheck, setResolveTimeCheck] = useState(0);
 
-    const [resolveTimeCheck, setResolveTimeCheck] = useState(0);
-
-
-        useEffect(() => {
+  const [userData, setUserData] = useState(store.get("userData"));
+  useEffect(() => {
     axios
-      .get("https://m2r31169.herokuapp.com/api/getresolveTime")
+      .get("https://m2r31169.herokuapp.com/api/getresolveTime", {
+        headers: {
+          "x-access-token": userData.accessToken, //the token is a variable which holds the token
+        },
+      })
       .then((res) => {
         console.log("resolveTime coming", res.data);
         if (
           res.data._1_to_9 !== null &&
           res.data._10_to_16 !== null &&
           res.data._17_to_24 !== null &&
-            res.data.geraterThan24 !== null
+          res.data.geraterThan24 !== null
         ) {
           setResolveTime({
             less: res.data._1_to_9,
             medium: res.data._10_to_16,
             high: res.data._17_to_24,
-              greaterThanTwentyFour: res.data.geraterThan24
+            greaterThanTwentyFour: res.data.geraterThan24,
           });
-        } else{
-            setResolveTimeCheck(1);
-            setResolveTime({
-                less: 0,
-                medium: 0,
-                high: 0,
-                greaterThanTwentyFour: 0
-            });
+        } else {
+          setResolveTimeCheck(1);
+          setResolveTime({
+            less: 0,
+            medium: 0,
+            high: 0,
+            greaterThanTwentyFour: 0,
+          });
         }
-
       })
       .catch((err) => console.error(err));
   }, []);
 
   const piechart = {
     data: {
-      series: [resolveTime.high, resolveTime.less, resolveTime.medium, resolveTime.greaterThanTwentyFour],
+      series: [
+        resolveTime.high,
+        resolveTime.less,
+        resolveTime.medium,
+        resolveTime.greaterThanTwentyFour,
+      ],
     },
     options: {
       labelInterpolationFnc: function (value) {
@@ -64,105 +73,103 @@ function ResolveTimeAnalysisStats() {
 
   return (
     <div className="resolve-main">
-        {resolveTimeCheck === 0 ?
+      {resolveTimeCheck === 0 ? (
         <ChartistGraph
-            data={piechart.data}
-            type="Pie"
-            options={piechart.options}
-            style={{ padding: "10px" }}
-            />
-        :
-            <div className="no-resolve-time-heading">
-                <p className="resolve-text">Not available</p>
-            </div>
-        }
+          data={piechart.data}
+          type="Pie"
+          options={piechart.options}
+          style={{ padding: "10px" }}
+        />
+      ) : (
+        <div className="no-resolve-time-heading">
+          <p className="resolve-text">Not available</p>
+        </div>
+      )}
 
       <div className="resolve-heading">
         <p className="resolve-text">Resolve Time Analysis</p>
 
+        <Box
+          textAlign="left"
+          color="#43a047"
+          fontWeight="bold"
+          component="span"
+          fontSize="3rem"
+        >
+          .
+        </Box>
+        <Box
+          textAlign="left"
+          color="#008081"
+          fontWeight="600"
+          component="span"
+          fontSize="0.75rem"
+        >
+          {" "}
+          1 to 9 hours
+        </Box>
+        <Box
+          textAlign="left"
+          color=" #f4c63d"
+          fontWeight="bold"
+          component="span"
+          fontSize="3rem"
+          marginLeft={1}
+        >
+          .
+        </Box>
+        <Box
+          textAlign="left"
+          color="#008080"
+          fontWeight="600"
+          component="span"
+          fontSize="0.75rem"
+        >
+          {" "}
+          10 to 16 hours
+        </Box>
+        <Box
+          textAlign="left"
+          color="#fb8c00"
+          fontWeight="bold"
+          component="span"
+          fontSize="3rem"
+          marginLeft={1}
+        >
+          .
+        </Box>
+        <Box
+          textAlign="left"
+          color="#008080"
+          fontWeight="600"
+          component="span"
+          fontSize="0.75rem"
+        >
+          {" "}
+          17 to 24 hours
+        </Box>
 
-      <Box
-        textAlign="left"
-        color="#43a047"
-        fontWeight="bold"
-        component="span"
-        fontSize="3rem"
-      >
-        .
-      </Box>
-      <Box
-        textAlign="left"
-        color="#008081"
-        fontWeight="600"
-        component="span"
-        fontSize="0.75rem"
-      >
-        {" "}
-        1 to 9 hours
-      </Box>
-      <Box
-        textAlign="left"
-        color=" #f4c63d"
-        fontWeight="bold"
-        component="span"
-        fontSize="3rem"
-        marginLeft={1}
-      >
-        .
-      </Box>
-      <Box
-        textAlign="left"
-        color="#008080"
-        fontWeight="600"
-        component="span"
-        fontSize="0.75rem"
-      >
-        {" "}
-        10 to 16 hours
-      </Box>
-      <Box
-        textAlign="left"
-        color="#fb8c00"
-        fontWeight="bold"
-        component="span"
-        fontSize="3rem"
-        marginLeft={1}
-      >
-        .
-      </Box>
-      <Box
-        textAlign="left"
-        color="#008080"
-        fontWeight="600"
-        component="span"
-        fontSize="0.75rem"
-      >
-        {" "}
-        17 to 24 hours
-      </Box>
-
-          <Box
-              textAlign="left"
-              color="#FF0000"
-              fontWeight="bold"
-              component="span"
-              fontSize="3rem"
-              marginLeft={1}
-          >
-              .
-          </Box>
-          <Box
-              textAlign="left"
-              color="#008080"
-              fontWeight="600"
-              component="span"
-              fontSize="0.75rem"
-          >
-              {" "}
-              more than 24 hours
-          </Box>
-
-        </div>
+        <Box
+          textAlign="left"
+          color="#FF0000"
+          fontWeight="bold"
+          component="span"
+          fontSize="3rem"
+          marginLeft={1}
+        >
+          .
+        </Box>
+        <Box
+          textAlign="left"
+          color="#008080"
+          fontWeight="600"
+          component="span"
+          fontSize="0.75rem"
+        >
+          {" "}
+          more than 24 hours
+        </Box>
+      </div>
     </div>
   );
 }
