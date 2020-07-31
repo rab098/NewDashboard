93a42b43e02757cb7eba7ab7f904f3f9071e9d79
Maha <tausif.maha@gmail.com>
2020-07-31 12:19:56 +0500

    report in progress

--- src/Components/Reports/GenerateReports.js
+++ src/Components/Reports/GenerateReports.js
@@ -7,6 +7,11 @@ import PdfMakeTable from "./PdfMakeTable";
 import TextField from '@material-ui/core/TextField';
 import 'date-fns';
 import DateFnsUtils from "@date-io/date-fns";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Paper from '@material-ui/core/Paper';
 import Moment from "moment";
 
 // import MomentUtils from '@date-io/moment';
@@ -28,12 +33,43 @@ import {
     PDFColumn
 } from 'react-pdfmake';
 import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
 
 // const ref = React.createRef();
 
 let store = require("store");
 
+
+const styles = {
+    button: {
+        marginTop: 20,
+        marginRight: 20,
+    },
+    actionsContainer: {
+        marginBottom: 20,
+    },
+    resetContainer: {
+        padding: 20,
+
+    },
+
+    stepperBg:{
+        backgroundColor: "transparent",
+    },
+
+    step:{
+        color: "#008080"
+    },
+};
+
+const useStyles = makeStyles(styles);
+
 function GenerateReports(props) {
+
+    const classes = useStyles();
+
 
     const [userData, setUserData] = useState(store.get("userData"));
 
@@ -47,6 +83,80 @@ function GenerateReports(props) {
 
     const [enableToDatePicker,setEnableToDatePicker] = useState(true)
 
+    const [activeStep, setActiveStep] = useState(0)
+    const steps = getSteps()
+
+    function getSteps() {
+        return ['Select the dates', 'Choose the type of complaint', 'Select the supervisor'];
+    }
+
+    function getStepContent(stepIndex) {
+        switch (stepIndex) {
+            case 0:
+                return (<div>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                            margin="normal"
+                            defaultValue="20 Jul 2020"
+                            id="date-picker-dialog"
+                            label="From"
+                            format="dd MMM yyyy"
+                            error={false}
+                            minDate={firstDate}
+                            maxDate={lastDate}
+                            value={fromDate}
+                            onChange={handleFromDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                            TextFieldComponent={TextFieldComponent}
+
+                        />
+                    </MuiPickersUtilsProvider>
+
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                            margin="normal"
+                            id="date-picker-dialog"
+                            label="To"
+                            error={false}
+                            format="dd MMM yyyy"
+                            minDate={fromDate + (24*60*60)}
+                            maxDate={lastDate}
+                            value={toDate}
+                            onChange={handleToDateChange}
+                            disabled = {enableToDatePicker}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                            TextFieldComponent={TextFieldComponent}
+
+                        />
+                    </MuiPickersUtilsProvider>
+                </div>)
+            case 1:
+                return (<div>
+
+                </div>)
+            case 2:
+                return 'This is the bit I really care about!';
+            default:
+                return 'Unknown stepIndex';
+        }
+    }
+
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
 
     const handleFromDateChange = (date) => {
         setFromDate(date);
@@ -219,49 +329,64 @@ function GenerateReports(props) {
 
             </div>
 
+
+
             <div>
 
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <KeyboardDatePicker
-                        margin="normal"
-                        defaultValue="20 Jul 2020"
-                        id="date-picker-dialog"
-                        label="From"
-                        format="dd MMM yyyy"
-                        error={false}
-                        minDate={firstDate}
-                        maxDate={lastDate}
-                        value={fromDate}
-                        onChange={handleFromDateChange}
-                        KeyboardButtonProps={{
-                            'aria-label': 'change date',
-                        }}
-                        TextFieldComponent={TextFieldComponent}
+                    <Stepper className={classes.stepperBg} activeStep={activeStep} orientation="vertical">
+                        {steps.map((label, index) => (
+                            <Step  key={label}>
+                                <StepLabel>{label}</StepLabel>
+                                <StepContent>
+                                    <Typography>{getStepContent(index)}</Typography>
+                                    <div className={classes.actionsContainer}>
+                                        <div>
+                                            <Button
+                                                disabled={activeStep === 0}
+                                                onClick={handleBack}
+                                                className={classes.button}
+                                            >
+                                                Back
+                                            </Button>
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                onClick={handleNext}
+                                                className={classes.button}
+                                            >
+                                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </StepContent>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    {activeStep === steps.length && (
+                        <Paper square elevation={0} className={classes.resetContainer}>
+                            <Typography>All steps completed - you&apos;re finished</Typography>
+                            <Button onClick={handleReset} className={classes.button}>
+                                Reset
+                            </Button>
+                        </Paper>
+                    )}
 
-                    />
-                </MuiPickersUtilsProvider>
 
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <KeyboardDatePicker
-                        margin="normal"
-                        id="date-picker-dialog"
-                        label="To"
-                        error={false}
-                        format="dd MMM yyyy"
-                        minDate={fromDate + (24*60*60)}
-                        maxDate={lastDate}
-                        value={toDate}
-                        onChange={handleToDateChange}
-                        disabled = {enableToDatePicker}
-                        KeyboardButtonProps={{
-                            'aria-label': 'change date',
-                        }}
-                        TextFieldComponent={TextFieldComponent}
 
-                    />
-                </MuiPickersUtilsProvider>
             </div>
 
+            {/*<div>*/}
+            {/*    <Button*/}
+            {/*        disabled={activeStep === 0}*/}
+            {/*        onClick={handleBack}*/}
+
+            {/*    >*/}
+            {/*        Back*/}
+            {/*    </Button>*/}
+            {/*    <Button variant="contained" color="primary" onClick={handleNext}>*/}
+            {/*        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}*/}
+            {/*    </Button>*/}
+            {/*</div>*/}
 
             {/*<div className="report-filter-main-pdf">*/}
             {/*    <header >*/}
