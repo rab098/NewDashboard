8f379d036b86d78dfdf5cb2902f8ba7d1740f440
rab098 <rabiasehar98@gmail.com>
2020-08-19 11:58:00 +0500

    supervisor

--- src/Components/Supervisor/DeleteDialog.js
+++ src/Components/Supervisor/DeleteDialog.js
@@ -2,11 +2,14 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-import useState from "react";
+import { useState, useEffect } from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { IconButton } from "@material-ui/core";
 export default function DeleteDialog(props) {
   const [open, setOpen] = React.useState(false);
 
@@ -20,7 +23,6 @@ export default function DeleteDialog(props) {
   };
 
   const handleSent = () => {
-    console.log("yeieiei" + props.superId);
     props.onDelete(props.superId);
     setOpen(false);
   };
@@ -28,6 +30,7 @@ export default function DeleteDialog(props) {
     <div>
       <IconButton style={{ backgroundColor: "transparent" }}>
         <DeleteIcon
+          //   onClick={handleDelete(row.supervisorId)}
           onClick={handleClickOpen}
           style={{
             color: "#008080",
@@ -38,6 +41,10 @@ export default function DeleteDialog(props) {
         />
       </IconButton>
 
+      {/* 
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button> */}
       <Dialog
         open={open}
         onClose={handleClose}
@@ -47,6 +54,13 @@ export default function DeleteDialog(props) {
         <DialogTitle id="alert-dialog-title">
           {"Are you sure you want to delete this row?"}
         </DialogTitle>
+        {/* <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        */}
 
         <DialogActions>
           <Button onClick={handleClose} color="primary">
