e873ca80406d0775ff7561e8769275cfd6f1ab07
memongodil <memongodil@gmail.com>
2020-08-25 17:09:37 +0500

    Small Changes

--- src/Components/Complaint/Complaints.js
+++ src/Components/Complaint/Complaints.js
@@ -433,7 +433,6 @@ export default function Complaints(props) {
 
   //fetch and simplify object
   async function fetchData() {
-    props.handleError("503");
     var finalObj = [];
     axios
       .get("https://m2r31169.herokuapp.com/api/getComplaints", {
