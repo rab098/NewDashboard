c0c217dcc9caf4c2b0791cda5b600eee8e17d81b
rab098 <rabiasehar98@gmail.com>
2020-10-08 12:49:18 +0500

    ..

---
+++ client/build/precache-manifest.80aa38dec4456e67079ae7e4c81c2146.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e35a04980145224d6f629700d6c316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0734e2f623578a609820",
+    "url": "/static/css/2.64b0dc94.chunk.css"
+  },
+  {
+    "revision": "41a45a6d7d169db27714",
+    "url": "/static/css/main.c122efa3.chunk.css"
+  },
+  {
+    "revision": "0734e2f623578a609820",
+    "url": "/static/js/2.e52c0e97.chunk.js"
+  },
+  {
+    "revision": "5f1eb31c12884aa708c1cf5ff6145390",
+    "url": "/static/js/2.e52c0e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a45a6d7d169db27714",
+    "url": "/static/js/main.7c63b651.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e42184ccd1a1e6278dfc19802fead6dc",
+    "url": "/static/media/app_icon_without_bg.e42184cc.png"
+  },
+  {
+    "revision": "c2063f5ae3d0b199e4da2a76e59e3e7c",
+    "url": "/static/media/camera.c2063f5a.webp"
+  }
+]);
